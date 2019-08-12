a98c2df85eff35ee6c166629662b7bbcaee27c54
Ersin CEYLAN <ecyln26@gmail.com>
2019-08-12 13:49:29 +0300

    Constructor Injection with XML Configuration
    Dependency Injection - { Constructor Injection } with XML Configuration

--- spring-boot/src/com/examples/spring/BaseballCoach.java
+++ spring-boot/src/com/examples/spring/BaseballCoach.java
@@ -2,9 +2,20 @@ package com.examples.spring;
 
 public class BaseballCoach implements Coach{
 
+	private FortuneService fortuneService;
+	
+	public BaseballCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
 	@Override
 	public String getDailyWorkout() {
 		return "Spend 30 min. on batting practice";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
--- spring-boot/src/com/examples/spring/Coach.java
+++ spring-boot/src/com/examples/spring/Coach.java
@@ -3,5 +3,6 @@ package com.examples.spring;
 public interface Coach {
 
 	public String getDailyWorkout();
+	public String getDailyFortune();
 	
 }
