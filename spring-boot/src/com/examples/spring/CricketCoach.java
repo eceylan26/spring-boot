994c6d79003a5fc61abd449d78c07324cf4fcf29
Ersin CEYLAN <ecyln26@gmail.com>
2019-08-12 14:17:06 +0300

    Injecting Literal Values with XML Configuration
    Spring Boot - Injecting Literal Values with XML Configuration

--- spring-boot/src/com/examples/spring/CricketCoach.java
+++ spring-boot/src/com/examples/spring/CricketCoach.java
@@ -4,6 +4,28 @@ public class CricketCoach implements Coach {
 
 	private  FortuneService fortuneService;
 	
+	//add new fields for email and team
+	private String email;
+	private String team;
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String emailString) {
+		System.out.println("CricketCoach: inside setter method - setEmail");
+		this.email = emailString;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter method - setTeam");
+		this.team = team;
+	}
+	
 	public CricketCoach() {
 		System.out.println("CricketCoach: inside default constructor");
 	}
