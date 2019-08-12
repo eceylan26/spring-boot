610b82f3b075e669ee01617eae1fba93c3577c3c
Ersin CEYLAN <ecyln26@gmail.com>
2019-08-12 23:35:25 +0300

    Singleton Bean Scope
    Spring Boot - Singleton Bean Scope

---
+++ spring-boot/src/com/examples/spring/BeanScopeDemoApp.java
@@ -0,0 +1,31 @@
+package com.examples.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("file:src/com/examples/spring/beanScope-applicationContext.xml");
+		
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach",Coach.class);
+		
+		Coach alphaCoach = context.getBean("myCoach",Coach.class);
+		
+		// check if they are the same bean
+		boolean result = (theCoach==alphaCoach);
+
+		// print the result
+		System.out.println("Pointing to the same object ---> " + result);
+
+		System.out.println("Memory location of theCoach ---> " + theCoach);
+		
+		System.out.println("Memory location of alphaCoach ---> " + theCoach);
+		
+		context.close();
+	}
+
+}
